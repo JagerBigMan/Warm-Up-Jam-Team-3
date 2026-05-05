8113bb681c8369709a9d88d3bcfeb797809c124f
Johnson Doong <156211561+JagerBigMan@users.noreply.github.com>
2026-05-05 16:04:10 -0400

    added ammo count

---
+++ Assets/Scripts/BulletCountUI.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletCountUI : MonoBehaviour
+{
+    public AutoRotatingTurretShooter shooter;
+
+    public GameObject bulletIconPrefab;
+    public Transform bulletIconParent;
+
+    public Color fullColor = Color.white;
+    public Color emptyColor = new Color(1f, 1f, 1f, 0.25f);
+
+    private List<Image> bulletIcons = new List<Image>();
+
+    void Start()
+    {
+        CreateBulletIcons();
+        UpdateAmmoUI();
+    }
+
+    void Update()
+    {
+        UpdateAmmoUI();
+    }
+
+    void CreateBulletIcons()
+    {
+        for (int i = 0; i < shooter.maxAmmo; i++)
+        {
+            GameObject icon = Instantiate(bulletIconPrefab, bulletIconParent);
+            Image image = icon.GetComponent<Image>();
+
+            if (image != null)
+            {
+                bulletIcons.Add(image);
+            }
+        }
+    }
+
+    void UpdateAmmoUI()
+    {
+        if (shooter == null) return;
+
+        for (int i = 0; i < bulletIcons.Count; i++)
+        {
+            bulletIcons[i].color = i < shooter.currentAmmo ? fullColor : emptyColor;
+        }
+    }
+}
